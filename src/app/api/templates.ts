164093a4ab70098133d12ec70e1d7824774f3d11
ahmed11102001 <a1110hmedadel@gmail.com>
2026-04-13 01:51:57 +0200

    Update templates.ts

--- src/app/api/templates.ts
+++ src/app/api/templates.ts
@@ -16,8 +16,7 @@ let templates = [
       content: "Hello 👋 this is a test message"
     }
   ];
-  
-  export default function handler(req, res) {
+  export default function handler(req: any, res: any) {
     if (req.method === "GET") {
       return res.status(200).json(templates);
     }
